1b951d834fa3abddb415a5d42ffb0c6e128736fd
thanks-cohn <officialmahasbiz@gmail.com>
2026-09-07 19:53:46 -0500

    Configure Astro for GitHub Pages

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://thanks-cohn.github.io',
+  base: '/underwater-school-aesthethic',
+  output: 'static',
+  trailingSlash: 'always'
+});
